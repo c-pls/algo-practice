0ff421f55c79f148fa12c5a8c221974c0686d566
c-pls <iamdchinh@gmail.com>
2025-01-07 21:32:34 +0700

    refactor two_sum function for improved readability and efficiency

--- src/leetcode/1.two_sum/two_sum.rs
+++ src/leetcode/1.two_sum/two_sum.rs
@@ -6,15 +6,10 @@ impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
         let mut m: HashMap<i32, usize> = HashMap::new();
         for (idx, &num) in nums.iter().enumerate() {
-            match m.get(&(target - num)) {
-                Some(&i) => {
-                    let mut res = vec![idx as i32, i as i32];
-                    res.sort();
-                    return res;
-                }
-                _ => {
-                    m.insert(num, idx);
-                }
+            if let Some(&i) = m.get(&(target - num)) {
+                return vec![idx as i32, i as i32];
+            } else {
+                m.insert(num, idx);
             }
         }
 
@@ -41,13 +36,14 @@ fn main() {}
 
 #[cfg(test)]
 mod tests {
-    use super::Solution; 
+    use super::Solution;
 
     #[test]
     fn test_two_sum_case_1() {
         let nums = vec![2, 7, 11, 15];
         let target = 9;
-        let result = Solution::two_sum(nums, target);
+        let mut result = Solution::two_sum(nums, target);
+        result.sort();
         assert_eq!(result, vec![0, 1]); // nums[0] + nums[1] = 9
     }
 
@@ -55,7 +51,8 @@ mod tests {
     fn test_two_sum_case_2() {
         let nums = vec![3, 2, 4];
         let target = 6;
-        let result = Solution::two_sum(nums, target);
+        let mut result = Solution::two_sum(nums, target);
+        result.sort();
         assert_eq!(result, vec![1, 2]); // nums[1] + nums[2] = 6
     }
 
@@ -63,7 +60,8 @@ mod tests {
     fn test_two_sum_case_3() {
         let nums = vec![3, 3];
         let target = 6;
-        let result = Solution::two_sum(nums, target);
+        let mut result = Solution::two_sum(nums, target);
+        result.sort();
         assert_eq!(result, vec![0, 1]); // nums[0] + nums[1] = 6
     }
 
@@ -71,7 +69,8 @@ mod tests {
     fn test_two_sum_no_solution() {
         let nums = vec![1, 2, 3];
         let target = 7;
-        let result = Solution::two_sum(nums, target);
+        let mut result = Solution::two_sum(nums, target);
+        result.sort();
         assert_eq!(result, vec![]); // No solution exists
     }
 }

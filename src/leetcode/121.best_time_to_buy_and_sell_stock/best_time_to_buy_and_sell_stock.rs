a259b580fa0240fc2fdb1d99a94d55e01ca683cb
c-pls <iamdchinh@gmail.com>
2025-01-09 23:37:36 +0700

    add solution for best time to buy and sell stock problem rust version

---
+++ src/leetcode/121.best_time_to_buy_and_sell_stock/best_time_to_buy_and_sell_stock.rs
@@ -0,0 +1,54 @@
+struct Solution;
+impl Solution {
+    pub fn max_profit(prices: Vec<i32>) -> i32 {
+        if prices.is_empty() {
+            return 0;
+        }
+
+        let mut max_profit = 0;
+        let mut min_price = prices[0];
+
+        for &price in prices.iter() {
+            if price < min_price {
+                min_price = price;
+                continue;
+            }
+
+            if price - min_price > max_profit {
+                max_profit = price - min_price
+            }
+
+        }
+        max_profit
+    }
+}
+fn main() {
+    let prices = vec![7, 1, 5, 3, 6, 4];
+    Solution::max_profit(prices);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    // Write your tests here
+    #[test]
+    fn test_case_1() {
+        let prices = vec![7, 1, 5, 3, 6, 4];
+        let result = 5;
+        assert_eq!(result, Solution::max_profit(prices));
+    }
+
+    #[test]
+    fn test_case_2() {
+        let prices = vec![7, 6, 4, 3, 1];
+        let result = 0;
+        assert_eq!(result, Solution::max_profit(prices));
+    }
+    #[test]
+    fn test_case_3_empty_prices() {
+        let prices = vec![];
+        let result = 0;
+        assert_eq!(result, Solution::max_profit(prices));
+    }
+}

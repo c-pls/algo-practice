d37b7e0bb20bd468d9b387adfe8f9b6db35d6346
c-pls <iamdchinh@gmail.com>
2025-05-11 10:54:14 +0700

    add solution for ransom note problem in Python

---
+++ src/leetcode/383.ransom_note/ransom_note.py
@@ -0,0 +1,5 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        from collections import Counter
+
+        return Counter(magazine) >= Counter(ransomNote)
